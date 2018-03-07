a1088482fc4cd1713a70dc956c233976cb088d71
Danilo Neri <daniloneri47@yahoo.com.br>
2018-03-07 00:26:52 -0300

    PUT, DELETE e GET para Clientes

---
+++ src/main/java/com/daniloneri/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,57 @@
+package com.daniloneri.cursomc.dto;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.daniloneri.cursomc.domain.Cliente;
+
+public class ClienteDTO {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, message = "O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
+	private String email;
+
+	public ClienteDTO() {
+
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
